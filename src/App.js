b1d49a48b253320f61a7263b710b84a8f755ac98
elenavedaleva <elenavedaleva@gmail.com>
2022-02-13 15:20:37 -0500

    implemented dropdown component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
     title: 'What is React?',
-    content: 'React is a front end javascript framework'
+    content: 'React is a front end javascript framework',
   },
   {
     title: 'Why use React?',
-    content: 'React is a favorite javascript library among engineers'
+    content: 'React is a favorite javascript library among engineers',
   },
   {
     title: 'How do you use React?',
-    content: 'You use React by creating components'
-  }
+    content: 'You use React by creating components',
+  },
+];
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'The Shade of Blue',
+    value: 'blue',
+  },
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
-    <div className="ui container" style={{marginTop: '20px'}}>
-      <Search />
+    <div className="ui container" style={{ marginTop: '20px' }}>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
