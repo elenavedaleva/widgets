da0bc5476b35c506f2eecaa60d14bd62568ee1bb
elenavedaleva <elenavedaleva@gmail.com>
2022-02-15 10:39:07 -0500

    implemented translate component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -34,15 +35,16 @@ const options = [
 ];
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
+  // const [selected, setSelected] = useState(options[0]);
 
   return (
     <div className="ui container" style={{ marginTop: '20px' }}>
-      <Dropdown
+      {/* <Dropdown
         selected={selected}
         onSelectedChange={setSelected}
         options={options}
-      />
+      /> */}
+      <Translate />
     </div>
   );
 };
